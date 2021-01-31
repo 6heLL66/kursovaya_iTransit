5c0c843c279b6b89643f1e6ee8779b74395565fb
6hell66 <sseniknik@gmail.com>
2021-01-31 05:59:15 +0300

    fix

--- client/src/SignInPage/SignInPage.js
+++ client/src/SignInPage/SignInPage.js
@@ -16,11 +16,11 @@ export default function SignInPage() {
 
     useEffect(() => {
         window.VK.Widgets.Auth('vk_auth', {
-            onAuth: (user) => login("vk", user, lang, theme)
+            onAuth: (user) => login("vk", user)
         })
-    }, [lang, theme])
+    }, [])
 
-    async function login(type, user, lang, theme) {
+    const login = useCallback(async (type, user) => {
         try {
             const data = await request(
                 "/api/auth/login",
@@ -44,7 +44,7 @@ export default function SignInPage() {
         } catch (e) {
             console.log(e.message)
         }
-    }
+    }, [theme, lang])
     return (
         <Container className={"mt-4 py-5" + " bg-" + (theme === "dark" ? "semi-dark" : "white")
         + " text-" + (theme === "dark" ? "white-50" : "dark")}>
@@ -86,7 +86,7 @@ export default function SignInPage() {
                         <GoogleLogin
                             clientId="808424836478-45hk3q9s4jbhlc9thn8sud56g7ff1u3m.apps.googleusercontent.com"
                             redirectUri={"https://itransitkurs.herokuapp.com/"}
-                            onSuccess={(res) => login("google", res.profileObj, lang, theme)}
+                            onSuccess={(res) => login("google", res.profileObj)}
                             cookiePolicy={'single_host_origin'}
                         />
                     </Row>
